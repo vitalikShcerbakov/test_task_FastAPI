ef4bdb990b16852006070f9699309131d077cf06
vitalik <sandwich.htc@gmail.com>
2024-01-25 21:37:46 +0300

    Added authorization

--- client.py
+++ client.py
@@ -1,16 +1,36 @@
-import requests
 import json
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+USERNAME = "johndoe"
+PASSWORD = "secret"
+
+
+def get_token():
+    token_url = "http://localhost:8000/token"
+    auth = HTTPBasicAuth(username=USERNAME, password=PASSWORD)
+    data = {
+        "grant_type": "password",
+        "username": USERNAME,
+        "password": PASSWORD,
+    }
+    response = requests.post(token_url, auth=auth, data=data)
+    return response.json().get("access_token")
 
 
 def main():
+    token = get_token()
+    headers = {"Authorization": f"Bearer {token}"}
+
     for i in range(10):
         requests.post(
             'http://127.0.0.1:8000/item/',
             data=json.dumps(
                 {'value': f'test_client {i}'}
-            ))
+            ), headers=headers)
 
-    response = requests.get('http://127.0.0.1:8000/items/')
+    response = requests.get('http://127.0.0.1:8000/items/', headers=headers)
     print(response.text)
 
 
